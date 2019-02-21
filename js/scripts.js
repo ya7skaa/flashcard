d40137eaf09d5663d20085aedbe5b02916797565
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-02-21 11:43:39 +0300

    javascript function to hide added

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#heading1").click(function(){
+    $("#hidden1").slideDown();
+  });
+  $("#heading2").click(function(){
+    $("#hidden2").slideDown();
+  });
+  $("#heading3").click(function(){
+    $("#hidden3").slideDown();
+  });
+  $("#heading4").click(function(){
+    $("#hidden4").slideDown();
+  });
+});
